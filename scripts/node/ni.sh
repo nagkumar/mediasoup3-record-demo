ef35b26955c41e6eff58efcca600aaa353a1583e
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2021-03-30 15:34:48 +0530

    w

--- scripts/node/ni.sh
+++ scripts/node/ni.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+. ~/.nvm/nvm.sh
 nvm list
 nvm install $NODE_VER
 
