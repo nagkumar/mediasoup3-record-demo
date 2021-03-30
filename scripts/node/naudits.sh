c0e1ebf2769f3362bde2cddd1a275b8cbaef43f5
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2021-03-30 14:33:51 +0530

    convert more node installs into scripts

---
+++ scripts/node/naudits.sh
@@ -0,0 +1,12 @@
+npm-check
+npm list
+npm outdated --depth=3
+npm fund
+cost-of-modules
+download-size mediasou
+howfat mediasoup
+modclean -n default:safe,default:caution -r
+cd node_modules | node-prune
+
+#minify-all .
+#node --trace-deprecation node_modules/webpack/bin/webpack.js
--- scripts/node/ni.sh
+++ scripts/node/ni.sh
@@ -1,3 +1,5 @@
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+. ~/.nvm/nvm.sh
 nvm list
 nvm install v14.16.0
+
