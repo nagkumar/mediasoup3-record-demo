061cdb7135e576a23ebc6131736ab6d2ad394da3
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2021-03-30 14:52:17 +0530

    ws' batfiles

---
+++ docs/os/wsl/wse.bat
@@ -0,0 +1,3 @@
+call wsstop.bat
+wsl --export Ubuntu E:\downloads\Softwares\os\linux\wsl2\Ubentu20.tar
+call wsstart.bat
---
+++ docs/os/wsl/wsi.bat
@@ -0,0 +1,6 @@
+call wsstop.bat
+wsl --unregister Ubuntu
+wsl --import Ubuntu "C:\Users\Raja Nagendra Kumar" E:\downloads\Softwares\os\linux\wsl2\Ubentu20.tar
+call wslist.bat
+call wsstart.bat
+
