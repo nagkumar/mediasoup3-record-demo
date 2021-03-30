0fe91b0894a0b352959e966b28ad5225542ecb01
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2021-03-30 19:10:06 +0530

    qweqwe

---
+++ docs/os/linux/upd.sh
@@ -0,0 +1,14 @@
+sudo apt update && sudo apt -y upgrade
+sudo apt dist-upgrade
+sudo apt-get install -y  build-essential curl file git wget libssl-dev pkg-config net-tools snapd
+sudo apt-get install -y kubuntu-desktop #or sudo apt install ubuntu-desktop gnome
+sudo apt-get install xrdp
+sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
+sudo systemctl enable dbus
+sudo systemctl enable xrdp
+sudo /etc/init.d/dbus start
+sudo /etc/init.d/xrdp start
+sudo /etc/init.d/xrdp status
